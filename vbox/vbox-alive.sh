962839a09035879992415fee812fec110403149e
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-09-21 23:27:10 +0100

    Migrate vbox alive

--- vbox/vbox-alive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Muestra las máquinas virtuales "vivas" de PRO o DSW
-
-nmap -sP -R 10.103.1-26.20 10.109.3-15.20 10.109.17-28.20 | perl -nle 'print $1 if /Nmap scan report for (.*)/'
