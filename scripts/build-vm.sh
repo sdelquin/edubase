ef38f8f15f5425f5216012cde1f645a5ac306a98
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-09-19 16:54:34 +0100

    Ask ova download if exists

--- scripts/build-vm.sh
+++ scripts/build-vm.sh
@@ -20,9 +20,13 @@ OVA_NAME=daw.ova
 OVA_URL=$OVA_BASE_URL/$OVA_NAME
 OVA_LOCAL_PATH=$HOME/Descargas/$OVA_NAME
 
-# check if OVA is already saved
-if [ ! -f "$OVA_LOCAL_PATH" ]
+if [ -f "$OVA_LOCAL_PATH" ]
 then
+    read -p "Ya se ha descargado una OVA en la ruta $OVA_LOCAL_PATH. ¿Usar esta OVA? [s/n] " -r
+    echo
+    [[ $REPLY =~ ^[Ss]$ ]] || curl -L $OVA_URL -o $OVA_LOCAL_PATH
+else
     curl -L $OVA_URL -o $OVA_LOCAL_PATH
 fi
+
 VBoxManage import $OVA_LOCAL_PATH --vsys=0 --vmname=$VM_NAME && rm -f $OVA_LOCAL_PATH
