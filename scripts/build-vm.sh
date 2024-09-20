819300d32c87ea15cd80b0569a5f4d3dfa7ad446
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-09-20 10:22:35 +0100

    Add confirmation before building vm

--- scripts/build-vm.sh
+++ scripts/build-vm.sh
@@ -20,13 +20,26 @@ OVA_NAME=daw.ova
 OVA_URL=$OVA_BASE_URL/$OVA_NAME
 OVA_LOCAL_PATH=$HOME/Descargas/$OVA_NAME
 
+# DESCARGA DE LA OVA
 if [ -f "$OVA_LOCAL_PATH" ]
 then
-    read -p "Ya se ha descargado una OVA en la ruta $OVA_LOCAL_PATH. ¿Usar esta OVA? [s/n] " -r
-    echo
+    read -p "Ya se ha descargado una OVA en la ruta $OVA_LOCAL_PATH. ¿Usar esta OVA? [S/n]: "
+    REPLY=${REPLY:-S}
     [[ $REPLY =~ ^[Ss]$ ]] || curl -L $OVA_URL -o $OVA_LOCAL_PATH
 else
     curl -L $OVA_URL -o $OVA_LOCAL_PATH
 fi
 
+# INSTALACIÓN DE LA OVA
+if VBoxManage list vms | perl -nle 'print $1 if /"([^"]+)"/' | grep -E '^'"$VM_NAME"'$' > /dev/null
+then
+    read -p "Ya existe una máquina virtual con el nombre "$VM_NAME". Para continuar habrá que borrar esta máquina virtual. ¿Continuar? [S/n]: "
+    REPLY=${REPLY:-S}
+    if [[ $REPLY =~ ^[Ss]$ ]]
+    then
+        VBoxManage unregistervm $VM_NAME --delete && rm -fr "$HOME/VirtualBox VMs/$VM_NAME"
+    else
+        exit 0
+    fi
+fi
 VBoxManage import $OVA_LOCAL_PATH --vsys=0 --vmname=$VM_NAME && rm -f $OVA_LOCAL_PATH
